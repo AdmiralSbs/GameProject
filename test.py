e4bf266f42e6d7edf0c7b50821264f9bb1d6d540
rohanius <rohan@rohanchandra.com>
2018-11-23 17:27:39 -0500

    this is a test file

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ yscale = 50
 
 camera = gamebox.Camera(600,600)
 object = gamebox.from_color(300,550,'red',500,50)
-p1 = gamebox.from_color(250,540,'blue',10,10)
 
 def tick(keys):
     camera.clear('lightblue')
