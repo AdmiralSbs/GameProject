d0f0b7f405323877bec0e646d4cb884be2031a94
admiralsbs <sbs5000ap@gmail.com>
2018-11-23 15:52:12 -0500

    First commit

---
+++ game.py
@@ -0,0 +1,82 @@
+# Watkins, jmw4dx
+import pygame
+import gamebox
+import smartbox
+
+
+
+scale = 50
+stuff = {
+    0: None,
+    1: gamebox.from_color(0, 0, "red", scale, scale),
+    2: gamebox.from_color(0, 0, "blue", scale, scale),
+}
+locations = [
+    [2] * 10,
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [2, 0, 2, 0, 2, 0, 1, 2, 0, 2],
+    [2, 0, 2, 0, 2, 0, 1, 2, 0, 2],
+    [2, 0, 0, 0, 0, 0, 0, 0, 0, 2],
+    [2] * 10,
+]
+
+walls = smartbox.create_map_from_list(locations, stuff, scale, scale)
+max_width = len(locations) * scale
+max_height = smartbox.max_size(locations) * scale
+camera = gamebox.Camera(200, 200)
+player = gamebox.from_color(100, 100, "yellow", 10, 10)
+
+jump_ready = False
+# currently_jumping = False
+terminal_jump_velocity = -10
+terminal_fall_velocity = 15
+
+def tick(keys):
+    global jump_ready
+
+    # key_down =  or pygame.K_SPACE in keys
+    if pygame.K_w not in keys:
+        jump_ready = False
+
+    if jump_ready and pygame.K_w in keys:
+        if (player.speedy <= terminal_jump_velocity):
+            jump_ready = False
+        player.speedy -= 0.5 # - 0.1 * player.speedy
+
+        # currently_jumping = True
+        # print("True")
+    else:
+        player.speedy = min(terminal_fall_velocity, player.speedy + 1.5)
+
+
+    if pygame.K_a in keys:
+        player.move(-5, 0)
+    if pygame.K_d in keys:
+        player.move(5, 0)
+    # if pygame.K_w in keys:
+    #    player.move(0, -5)
+    # if pygame.K_s in keys:
+    #    player.move(0, 5)
+
+    player.move_speed()
+
+    for wall in walls:
+        if wall._color == pygame.Color("blue"):
+            if player.touches(wall):
+                player.move_to_stop_overlapping(wall)
+                if (player.top >= max_height):
+                    player.bottom = wall.top
+            if player.bottom == wall.top and player.right > wall.left and player.left < wall.right:
+                jump_ready = True
+
+    camera.x = min(max(player.x, camera.width / 2), max_width - camera.width / 2)
+    camera.y = min(max(player.y, camera.height / 2), max_height - camera.height / 2)
+
+    camera.clear("green")
+    for wall in walls:
+        smartbox.draw_object(wall, camera)
+    smartbox.draw_object(player, camera)
+    camera.display()
+
+
+gamebox.timer_loop(30, tick)
---
+++ smartbox.py
@@ -0,0 +1,51 @@
+# Watkins, jmw4dx
+# Yes, I just did this
+import gamebox
+
+
+def max_size(listy):
+    """Given a 2D list, find the length of the largest list within
+    :param listy: 2D list
+    :return: Length of longest list
+    """
+    return max([len(x) for x in listy])
+
+
+def draw_object(sprite, camera):
+    """Given a SpriteBox and Camera, determine whether the sprite is on screen (they are overlapping)
+    :param sprite:  A SpriteBox object as defined by gamebox
+    :param camera:  A Camera object as defined by gamebox
+    :return: None
+    """
+    if not isinstance(sprite, gamebox.SpriteBox):
+        raise Exception("First parameter must be SpriteBox")
+    if not isinstance(camera, gamebox.Camera):
+        raise Exception("Second parameter must be Camera")
+
+    checks = [sprite.left < camera.right, sprite.right > camera.left,
+              sprite.top < camera.bottom, sprite.bottom > camera.top]
+    if True in checks:
+        sprite.draw(camera)
+
+
+blocks = {0: gamebox.from_color(0, 0, "red", 50, 50)}
+
+
+def create_map_from_list(locations, dicty, xscale, yscale):
+    """Given a 2D list of id values and a dictionary containing a SpriteBox for each id, generate those SpriteBox objects
+    :param locations: 2D list of ids, each interior list is for one x value, each item in that list is for one y value
+    :param dicty: Matches each id with a SpriteBox to make a copy of
+    :param xscale: The difference in space between each item
+    :param yscale: The difference in space between each item
+    :return: A list containing all of the created SpriteBoxes
+    """
+    created = []
+    for column in range(len(locations)):
+        for row in range((len(locations[column]))):
+            iden = locations[column][row]
+            if iden not in dicty.keys():
+                raise Exception("Locations contained key not in dict")
+            if isinstance(dicty[iden], gamebox.SpriteBox):
+                created.append(dicty[iden].copy_at((column + 0.5) * xscale, (row + 0.5) * yscale))
+
+    return created
