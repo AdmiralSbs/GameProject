be41692451b1d8d2d001e54b99c38c3df824ceca
admiralsbs <sbs5000ap@gmail.com>
2018-11-29 17:02:55 -0500

    Item working

--- dialogue.py
+++ dialogue.py
@@ -1,3 +1,11 @@
 # Watkins, jmw4dx
+import gamebox
+
+
 class Dialogue:
-    def
+    exists = False
+
+    def __init__(self, x, y, w, h):
+        if Dialogue.exists: raise Exception("You can only have one Dialogue at a time!")
+        background = gamebox.from_color(x, y, "white", w, h)
+        lines = []
--- items.py
+++ items.py
@@ -2,32 +2,20 @@
 import gamebox
 import pygame
 
+
 class Item:
-    def __init__(self, name, style, weight, img, color):
+    def __init__(self, name, style, weight, object):
         self.name = name
         self.style = style
         self.weight = weight
-        if (img != None):
-            self.object = gamebox.from_image(0, 0, img)
-            color = None
-        else:
-            if color == None:
-                color = 'black'
-            self.object = gamebox.from_color(0, 0, color, 50, 50)
-
+        self.object = object
 
 
 class Player:
-    def __init__(self, name, move_list, img, color):
+    def __init__(self, name, move_list, object):
         self.name = name
         self.move = move_list
-        if (img != None):
-            self.object = gamebox.from_image(0, 0, img)
-            color = None
-        else:
-            if color == None:
-                color = 'black'
-            self.object = gamebox.from_color(0, 0, color, 50, 50)
+        self.object = object
 
 
 styles = ["weapon", "potion", "ability"]
@@ -35,7 +23,7 @@ names = ['Sean', 'Alexander']
 upsorn_moves = ['Piazza Ask', 'Live Code', 'Teach Lecture', 'Wholesome Email']
 enemy_moves = ['Vague Answer', 'People\'s United', 'PA Reject', 'LAB DESTROY']
 
-for_loop = Item("FOR_LOOP", 'item', '1', None,)
-while_loop = Item('WHILE_LOOP', 'item', '2', None,)
-dictionary = Item('DICT', 'item', '3', None,)
+for_loop = Item("FOR_LOOP", 'item', '1', gamebox.from_text(0, 0, "FOR", 12, "yellow", bold=True, italic=False))
+while_loop = Item('WHILE_LOOP', 'item', '2', None, )
+dictionary = Item('DICT', 'item', '3', None, )
 list = Item('LIST', 'item', '4', None)
